30c48092960e4c0d7d40c4b310cf67d3e28c7470
Taylor Zweigle <zweigletaylor@gmail.com>
2024-04-28 20:57:52 -0500

    styled login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -8,6 +8,8 @@ import Typography from "@mui/material/Typography";
 
 import { useLogin } from "../hooks/useLogin";
 
+import Card from "../core/card/Card";
+
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -21,18 +23,37 @@ const LoginPage = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <Stack direction="column" gap={2}>
-          <TextField label="Username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-          <TextField label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          {error && <Typography variant="body2">{error}</Typography>}
-          <Button type="submit" variant="contained" disabled={loading}>
-            Login
-          </Button>
-        </Stack>
-      </form>
-    </>
+    <div style={{ display: "flex", justifyContent: "center", padding: "24px" }}>
+      <div style={{ width: "512px" }}>
+        <Card>
+          <div style={{ padding: "24px" }}>
+            <form onSubmit={handleSubmit}>
+              <Stack direction="column" gap={2}>
+                <Typography variant="h6">Air Raid Football App</Typography>
+                <TextField
+                  label="Username"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  sx={{ backgroundColor: "background.paper" }}
+                />
+                <TextField
+                  label="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  sx={{ backgroundColor: "background.paper" }}
+                />
+                {error && <Typography variant="body2">{error}</Typography>}
+                <Button type="submit" variant="contained" disabled={loading}>
+                  Login
+                </Button>
+              </Stack>
+            </form>
+          </div>
+        </Card>
+      </div>
+    </div>
   );
 };
 
